a25d5b8e3872f7560eb9fec44810a424331751e2
ryanxchen <javascriptchenxt@gmail.com>
2018-05-31 23:29:02 +0800

    init-demo

---
+++ dep.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# npm run build
+
+# cd dist
+
+git init
+git add -A
+git commit -m 'init-demo'
+
+git push -f git@github.com:ryanschen/docs-mui-vue.git master:gh-pages
+
+cd -
