34b8f870d003ede2e05ecad9d2b07b50d1f9cb78
ryanxchen <javascriptchenxt@gmail.com>
2018-06-26 17:19:39 +0800

    feat: update picker

--- src/components/picker.vue
+++ src/components/picker.vue
@@ -6,24 +6,49 @@
       <div @click="$_confirm">{{ confirmButtonText }}</div>
     </div>
     <div class="sq-picker-body">
-      <div class="sq-picker-body-wrapper" ref="wrapper">
-        <div class="sq-picker-body-content" :style="{ transform: `translate3d(0px, ${translateY}px, 0px)`, transition: `transform ${transitionTime}s` }">
-          <div class="sq-picker-body-item" v-for="(item, index) in list" :key="index">{{ item }}</div>
-        </div>
-        <div class="sq-picker-loading-mask" :style="{'backgroundColor': loading ? 'rgba(255,255,255,1)' : ''}">
-          <div class="sq-picker-loading-icon" v-show="loading"></div>
-        </div>
-        <div class="sq-picker-active-line sq-picker-top-line sq-picker-bottom-line" v-show="!loading"></div>
-      </div>
+      <picker-item
+        v-for="(item, index) in currentColumns"
+        :key="index"
+        :data-list="item"
+        :list-index="index"
+        :value-key="valueKey"
+        @on-change="$_onChange"
+      />
+        <!-- v-for="(slot, index) in list"
+        :key="index"
+        :valueKey="valueKey"
+        :values="slot.values || []"
+        :text-align="slot.textAlign || 'center'"
+        :visible-item-count="visibleItemCount"
+        :class-name="slot.className"
+        :flex="slot.flex"
+        v-model="values[slot.valueIndex]"
+        :rotate-effect="rotateEffect"
+        :divider="slot.divider"
+        :content="slot.content"
+        :itemHeight="itemHeight"
+        :default-index="slot.defaultIndex" -->
+    </div>
+    <div class="sq-picker-loading-mask" v-show="loading">
+      <div class="sq-picker-loading-icon"></div>
     </div>
   </div>
 </template>
 
 <script>
+import PickerItem from './pickerItem'
+
 export default {
   name: 'sq-picker',
 
+  components: {
+    PickerItem
+  },
+
   props: {
+    value: {
+      type: Array
+    },
     cancelButtonText: {
       type: String,
       default: '取消'
@@ -44,173 +69,87 @@ export default {
       type: Boolean,
       default: false
     },
-    list: {
+    columns: {
       type: Array,
       default: () => []
+    },
+    valueKey: {
+      type: String,
+      default: ''
     }
   },
 
   data () {
     return {
-      temp: null, // 容器
-      translateY: 96,
-      transitionTime: 0,
-      startY: 0, // 起始值
-      moveY: 0, // 移动的距离
-      saveY: 96, // 缓存偏移量
-      index: 0, // 选中的下标
-      maxY: 0, // 最大偏移距离
-      minY: 0, // 最小偏移距离
-      currentValue: null, // 当前选中的值
-      rowHeight: 48, // 每行列的高度--为固定值
-      offset: 2, // 列表初始偏移量--为固定值
-      startTime: undefined,
-      points: []
+      isCascade: Object.prototype.toString(this.columns[0]) === '[object Object]' && Array.isArray(this.columns[0].children),
+      currentColumns: this.formateColumns(this.columns),
+      currentValue: []
     }
   },
 
   methods: {
+    formateColumns (columns) {
+      if (columns.length && Array.isArray(columns[0])) {
+        return columns
+      } else {
+        let arr = columns
+        if (Object.prototype.toString(arr[0]) === '[object Object]' && Array.isArray(arr[0].children)) {
+          let result = []
+          while (arr && arr[0] && Object.prototype.toString(arr[0]) === '[object Object]' && Array.isArray(arr[0].children)) {
+            result.push(arr)
+            arr = arr[0].children
+          }
+          return result
+        } else {
+          return [columns]
+        }
+      }
+    },
+    getChildrens () {
+      return this.$children.filter(item => item.$options.name === 'sq-picker-item')
+    },
     $_cancel () {
-      this.$emit('input', false)
       this.$emit('cancel', this.currentValue, this.index)
     },
     $_confirm () {
-      this.$emit('input', false)
       this.$emit('confirm', this.currentValue, this.index)
     },
-    $_setY (val) {
-      this.saveY = this.translateY = val
+    updateCurrentValue (val) {
+      this.currentValue.push(this.valueKey ? val[this.valueKey] : val)
     },
-    $_setCurrentValue (val) {
-      this.currentValue = val
-    },
-    $_start (event) {
-      if (this.loading) return
-      this.startY = event.changedTouches[0].pageY
-      this.moveY = 0
-      this.startTime = +new Date()
+    $_onChange ({ item, itemIndex, listIndex }) {
+      let lenth = this.currentColumns.length
+      const { valueKey } = this
 
-      event.stopPropagation()
-      event.preventDefault()
-    },
-    $_move (event) {
-      if (this.loading) return
-      if (this.transitionTime) {
-        this.transitionTime = 0
-      }
+      if (this.isCascade && listIndex < lenth) {
+        let len = listIndex
+        let newChildrenArr = JSON.parse(JSON.stringify(item.children))
 
-      this.moveY = event.changedTouches[0].pageY - this.startY
-      this.translateY = this.saveY + this.moveY
+        while (len < lenth && newChildrenArr.length > 0) {
+          const child = this.getChildrens()[len + 1]
+          const offset = child.offset
+          const rowHeight = child.rowHeight
 
-      this.startTime = +new Date()
-      this.points.push({ time: this.startTime, y: event.changedTouches[0].pageY })
-      if (this.points.length > 40) {
-        this.points.shift()
-      }
+          child.list = newChildrenArr
+          child.$_setY(offset * rowHeight)
+          child.maxY = (newChildrenArr.length + offset) * rowHeight
+          child.minY = (offset - newChildrenArr.length + 1) * rowHeight
+          this.currentValue[len + 1] = valueKey ? newChildrenArr[0][valueKey] : newChildrenArr
 
-      event.stopPropagation()
-      event.preventDefault()
-    },
-    $_stop (diff = 0) {
-      // 缓存上次translateY的值
-      this.saveY = this.translateY
-
-      // 获得list 下标
-      this.index = parseInt((this.offset * this.rowHeight - this.saveY - diff) / this.rowHeight)
-      if (this.index < 0) {
-        this.index = 0
-      } else if (this.index >= this.list.length) {
-        this.index = this.list.length - 1
-      }
-
-      // 当前位置与 标准情况下 下标距离值 进行对比， 不相同情况下，进行手动设置位置
-      if (this.saveY !== ((this.offset - this.index) * this.rowHeight)) {
-        this.transitionTime = 0.3
-        // 向下拉 超过显示情况下
-        if (this.saveY > 0 && this.saveY > this.offset * this.rowHeight) {
-          this.$_setY(this.offset * this.rowHeight)
-          this.$_setCurrentValue(this.list[0])
-          // 向上拉 超过显示情况下
-        } else if (this.saveY < 0 && this.saveY < this.minY) {
-          this.$_setY(this.minY)
-          this.$_setCurrentValue(this.list[this.list.length - 1])
-        } else {
-          this.$_setY((this.offset - this.index) * this.rowHeight)
-          this.$_setCurrentValue(this.list[this.index])
-        }
-      }
-    },
-    $_end (event) {
-      if (!this.startY || this.loading) return
-
-      const endY = event.changedTouches[0].pageY
-      const endTime = +new Date()
-      // 如果最后次move时间与end时间超过100ms，不添加惯性滑动
-      if (endTime - this.startTime > 100) {
-        this.$_stop()
-      } else {
-        if (Math.abs(endY - this.startY) > 10) {
-          const endPos = this.points.length - 1
-          let startPos = endPos
-          for (let i = endPos; i > 0 && this.startTime - this.points[i].time < 100; i--) {
-            startPos = i
-          }
-
-          if (startPos !== endPos) {
-            const ep = this.points[endPos]
-            const sp = this.points[startPos]
-            const t = ep.time - sp.time
-            const s = ep.y - sp.y
-            const v = s / t // 出手时的速度
-            const diff = v * 150 // 滑行 150ms,这里直接影响“灵敏度”
-            this.$_stop(diff)
-          } else {
-            this.$_stop()
-          }
-        } else {
-          this.$_stop()
+          newChildrenArr = (newChildrenArr[0].children && (JSON.parse(JSON.stringify(newChildrenArr[0].children)))) || []
+          len++
         }
       }
 
-      event.stopPropagation()
-      event.preventDefault()
-    },
-    init () {
-      this.temp.addEventListener('touchstart', this.$_start, false)
-      this.temp.addEventListener('touchmove', this.$_move, false)
-      this.temp.addEventListener('touchend', this.$_end, false)
-      this.maxY = (this.list.length + this.offset) * this.rowHeight
-      this.minY = (this.offset - this.list.length + 1) * this.rowHeight
+      this.currentValue[listIndex] = this.valueKey ? item[this.valueKey] : item
+
+      this.$emit('on-change', {
+        value: JSON.parse(JSON.stringify(this.currentValue)),
+        item: JSON.parse(JSON.stringify(item)),
+        itemIndex: itemIndex,
+        listIndex: listIndex
+      })
     }
-  },
-
-  watch: {
-    list: {
-      handler (val) {
-        if (this.currentValue === null && val[0]) {
-          this.currentValue = val[0]
-        }
-      },
-      immediate: true
-    },
-    currentValue (val, oldVal) {
-      if (val !== oldVal) {
-        this.$emit('on-change', val, this.index)
-      }
-    }
-  },
-
-  mounted () {
-    this.$nextTick(() => {
-      this.temp = this.$refs.wrapper
-      this.init()
-    })
-  },
-
-  beforeDestroy () {
-    this.temp.removeEventListener('touchstart', this.$_start)
-    this.temp.removeEventListener('touchmove', this.$_move)
-    this.temp.removeEventListener('touchend', this.$_end)
   }
 }
 </script>
@@ -229,6 +168,7 @@ $theme-color: #4a90e2;
 .sq-picker {
   text-align: center;
   background-color: #fff;
+  position: relative;
   &-header {
     display: flex;
     min-height: 50px;
@@ -255,64 +195,6 @@ $theme-color: #4a90e2;
     position: relative;
     display: flex;
     overflow: hidden;
-    &-wrapper {
-      flex: 1;
-      position: relative;
-      height: 100%;
-      .sq-picker-loading-mask {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: linear-gradient(180deg, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.6)),
-          linear-gradient(0deg, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.6));
-        background-position: top, bottom;
-        background-size: 100% 96px;
-        background-repeat: no-repeat;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      .sq-picker-active-line {
-        width: 100%;
-        height: 48px;
-        position: absolute;
-        left: 0;
-        top: 96px;
-      }
-      &-content {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        &-item {
-          height: 48px;
-          &.sq-picker-bottom-line {
-            &:last-child::after {
-              border: 0;
-            }
-          }
-        }
-      }
-    }
-  }
-  &-top-line {
-    position: relative;
-    &::before {
-      content: '';
-      position: absolute;
-      left: 0;
-      top: 0;
-      right: 0;
-      height: 1px;
-      border-bottom: 1px solid #E5E5E5;
-      color: #E5E5E5;
-      -webkit-transform-origin: 0 100%;
-      transform-origin: 0 100%;
-      -webkit-transform: scaleY(0.5);
-      transform: scaleY(0.5);
-    }
   }
   &-bottom-line {
     position: relative;
@@ -331,10 +213,22 @@ $theme-color: #4a90e2;
       transform: scaleY(0.5);
     }
   }
+  &-loading-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   &-loading-icon {
     box-sizing: border-box;
-    width: 36px;
-    height: 36px;
+    width: 1em;
+    height: 1em;
+    font-size: 36px;
     border: 3px solid transparent;
     border-top-color: $theme-color;
     border-left-color: $theme-color;
